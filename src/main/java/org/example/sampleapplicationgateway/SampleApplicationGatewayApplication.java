68ef6e49c4ed046cc01d1ea9ff7e78dd92f454cb
emmanuel-mutua <emmamulwa2020@gmail.com>
2024-05-14 17:28:30 +0300

    Initial commit

---
+++ src/main/java/org/example/sampleapplicationgateway/SampleApplicationGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.sampleapplicationgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplicationGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplicationGatewayApplication.class, args);
+    }
+
+}
